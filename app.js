08d3a69591c863aae3cfc81296e05917c4cd62b6
Anh Phan <anhphan@Anhs-MacBook-Pro.local>
2024-01-08 23:09:21 +0700

    Modify app.js

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
+require("dotenv").config();
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
-require("dotenv").config();
 
 var app = express();
 
