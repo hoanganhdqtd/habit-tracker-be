a26b05167ea4d84e7a963cafef673a711faa4650
Anh Phan <anhphan@Anhs-MacBook-Pro.local>
2024-03-17 16:15:27 +0700

    Fix cors issues

--- app.js
+++ app.js
@@ -29,8 +29,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // app.use(cors());
 app.use(
   cors({
-    // origin: process.env.DEPLOY_URL,
-    origin: "https://habit-tracker-968909.netlify.app",
+    origin: process.env.DEPLOY_URL,
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
